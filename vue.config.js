86626a5118096f606e347b3d1547670d683f3506
qsechq <94899568+qsechq@users.noreply.github.com>
2023-01-20 10:56:11 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/Antt.vue/' : '/' }
+module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/PostsList/' : '/' }
 // module.exports = {
 //   configureWebpack: {
 //     devtool: 'source-map'
